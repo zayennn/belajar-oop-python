55ffaeb199019d25b97e53dba8e82df7324c9b74
zayennn <zaayeennn@gmail.com>
2025-10-21 15:56:32 +0700

    update

--- latihan.py
+++ latihan.py
@@ -1,4 +1,5 @@
 # ============= latihan 1 =============
+print("\n============= latihan 1 =============\n")
 class Mahasiswa: 
     def __init__(self, name, nim, major):
         self.name = name
@@ -6,8 +7,7 @@ class Mahasiswa:
         self.major = major
         
     def info(self) :
-        print("\n============= latihan 1 =============\n")
-        print("============= Output : =============\n")
+        print("\n============= Output : =============\n")
         print(f"""nama mahasiswa                 : {self.name}
 nim maahasiswa                 : {self.nim}
 jurusan yang diambil mahasiswa : {self.major}""")
