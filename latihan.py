36e82afac2ad50bfcd3a1492d59b1ef37a57e72c
zayennn <zaayeennn@gmail.com>
2025-10-21 15:46:19 +0700

    update

--- latihan.py
+++ latihan.py
@@ -1,1 +1,10 @@
 # ============= latihan 1 =============
+class Mahasiswa: 
+    def __init__(self, name, nim, major):
+        self.name = name
+        self.nim = nim
+        self.major = major
+        
+    def info(self) :
+        print("============= latihan 1 =============")
+        print(f"")
