a27556feadf71c9b3c12c8167a6b4cd51b47c634
zayennn <zaayeennn@gmail.com>
2025-10-23 23:14:11 +0700

    update

--- latihan2.py
+++ latihan2.py
@@ -1,3 +1,10 @@
 n = int(input('masukan jumlah data : '))
 r = int(input('masukan jumlah data yang dipilih : '))
 
+hasil_n = n * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+hasil_r = r * 4 * 3 * 2 * 1
+
+hasil_n_dibagi_r = hasil_n / hasil_r
+
+print('hasil nya : ', hasil_n_dibagi_r)
