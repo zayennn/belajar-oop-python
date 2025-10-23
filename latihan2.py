662025974ce49d1c20273aa89cceafc06d86f1c7
zayennn <zaayeennn@gmail.com>
2025-10-23 23:20:50 +0700

    update

--- latihan2.py
+++ latihan2.py
@@ -1,10 +1,103 @@
-n = int(input('masukan jumlah data : '))
-r = int(input('masukan jumlah data yang dipilih : '))
+# latihan 1
+class Buah() :
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        
+    def info(self) :
+        print(f"""
+ini adalah buah : {self.name}
+warna buah ini adalah : {self.color}
+""")
+        
 
-hasil_n = n * 7 * 6 * 5 * 4 * 3 * 2 * 1
+apel = Buah("Apel", "Merah")
+apel.info()
 
-hasil_r = r * 4 * 3 * 2 * 1
 
-hasil_n_dibagi_r = hasil_n / hasil_r
+# latihan 2
+class PersegiPanjang() :
+    def __init__(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+    
+    def hitung_luas(self) : 
+        print(f"""
+luas = {self.panjang * self.lebar}  
+hitung keliling = {2 * (self.panjang + self.lebar)}        
+""")
+        
+panjang = PersegiPanjang(50, 70)
+panjang.hitung_luas()
 
-print('hasil nya : ', hasil_n_dibagi_r)
+
+# latihan 3
+class NilaiMahasiswa() :
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    
+    def check_kelulusan(self) :
+        print(f"""
+nama mahasiswa : {self.name}
+nilai mahasiswa : {self.score}
+status mahasiswa : {"lulus" if self.score >= 60 else "tidak lulus"}
+""")
+        
+nama_mahasiswa = input('masukan nama mahasiswa : ').capitalize()
+nilai = int(input('masukan nilai : '))
+
+mahasiswa = NilaiMahasiswa(nama_mahasiswa, nilai)
+mahasiswa.check_kelulusan()
+
+
+# latihan 4
+class Kendaraan() :
+    def __init__(self, merk, year):
+        self.merk = merk
+        self.year = year
+        
+class JumlahPintu (Kendaraan) :
+    def __init__(self, merk, year, jumlah_pintu):
+        super().__init__(merk, year)
+        
+        self.jumlah_pintu = jumlah_pintu
+        
+    def info_kendaraan(self) :
+        print(f"""
+merk kendaraan : {self.merk}
+tahun keluaran : {self.year}
+jumlah pintu mobil : {self.jumlah_pintu}              
+""")
+        
+        
+bmwm2 = JumlahPintu("BMW M2", "2024", 2)
+bmwm2.info_kendaraan()
+
+
+# latihan 5
+class Pegawai() :
+    def __init__(self, nama, gaji_pokok):
+        self.nama = nama
+        self.gaji_pokok = gaji_pokok
+        
+class Manager(Pegawai) :
+    def __init__(self, nama, gaji_pokok, tunjangan):
+        super().__init__(nama, gaji_pokok)
+        self.tunjangan = tunjangan
+        
+    def total_gaji(self) :
+        print(f"""
+nama : {self.nama}
+total gaji : {self.gaji_pokok}
+""")
+        
+        
+nama = input('masukan nama : ').capitalize()
+gaji_pokok = int(input('masukan gaji pokok : '))
+tunjangan = int(input('masukan tujangan : '))
+
+total_jumlah_gaji = gaji_pokok + tunjangan
+
+info = Manager(nama, total_jumlah_gaji, tunjangan)
+info.total_gaji()
