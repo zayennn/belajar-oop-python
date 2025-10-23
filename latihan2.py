faf41223d371f9acb4bdb77f1099315fd875e783
zayennn <zaayeennn@gmail.com>
2025-10-23 22:27:01 +0700

    update

--- latihan2.py
+++ latihan2.py
@@ -6,10 +6,26 @@ class Buah() :
         
     def info(self) :
         print(f"""
-    ini adalah buah : {self.name}
-    warna buah ini adalah : {self.color}
+ini adalah buah : {self.name}
+warna buah ini adalah : {self.color}
 """)
         
-        
+
 apel = Buah("Apel", "Merah")
 apel.info()
+
+
+# latihan 2
+class PersegiPanjang() :
+    def __init__(self, panjang, lebar):
+        self.panjang = panjang
+        self.lebar = lebar
+    
+    def hitung_luas(self) : 
+        print(f"""
+luas = {self.panjang * self.lebar}  
+hitung keliling = {2 * (self.panjang + self.lebar)}        
+""")
+        
+panjang = PersegiPanjang(50, 70)
+panjang.hitung_luas()
